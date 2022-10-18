e72e6232452316dbbf149220f1b17c5e7229dd69
georgiana.lingurariu-at-336537318274 <georgiana.lingurariu@endava.com>
2022-10-18 13:08:26 +0300

    Use HashRouter instead of BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import store from "./app/store";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -12,11 +12,11 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
